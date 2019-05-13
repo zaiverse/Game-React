1c8802b029f0eced89ccb20fac96a758e9159b90
Brenda Garcia <brenda.garcia007@mymdc.net>
2019-05-13 15:24:51 -0400

    Updates

---
+++ precache-manifest.f663d1a80f25ec1784f97d53d49922f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77a26e3a148af3a861dde46ba756d34",
+    "url": "/Game-React/index.html"
+  },
+  {
+    "revision": "1f8b702cb3ce3d55c152",
+    "url": "/Game-React/static/css/main.a298010d.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Game-React/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1f8b702cb3ce3d55c152",
+    "url": "/Game-React/static/js/main.c51de738.chunk.js"
+  },
+  {
+    "revision": "35510a3b08332016c2ca",
+    "url": "/Game-React/static/js/runtime~main.f40dde1b.js"
+  }
+]);
